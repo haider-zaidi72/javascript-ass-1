00c1c18d0ac564db280dc6fae0c9b6288a15d0b8
urooj haider <suhz1100@gmail.com>
2025-05-14 14:10:14 +0500

    add

---
+++ java.js
@@ -0,0 +1,12 @@
+
+alert("Welcome to Our Website!!");
+
+let password = 1234
+let userInput = +prompt("Pleae enter the password");
+if(userInput !== password){
+  alert("Error! Enter the valid password");
+}
+
+alert("welcome to Js land...\nHappy coding")
+
+alert("welcome to JS land");
